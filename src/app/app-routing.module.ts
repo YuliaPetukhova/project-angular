34fa00e1bd139841081c871ccc1ff6a7305a03dc
YuliaPetukhova <yuljaschik99@gmail.com>
2023-10-30 16:51:02 +0500

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
 
+
+const routes: Routes = [
+  // { path: '', component: ProductPageComponent},
+  { path: 'main', component: MainPageComponent}
+];
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+// import { ProductPageComponent } from './pages/product-page/product-page.component';
+
