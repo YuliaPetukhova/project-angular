97367b5d1e6eaa4894f7058b5b926705f15bb2a5
YuliaPetukhova <yuljaschik99@gmail.com>
2023-10-31 13:36:53 +0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ModalComponent } from './components/modal/modal/modal.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,20 +6,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/pages/main-page/main-page.component';
 import { TaskPageComponent } from './components/pages/tasks-page/task-page.component';
-import {FormControl, Validators} from '@angular/forms';
-
+import {MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog'
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     TaskPageComponent,
+    ModalComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormControl,
-    Validators,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
