a6d7562d91ac7016d8f6d28d09d3074fa0857038
YuliaPetukhova <yuljaschik99@gmail.com>
2023-10-31 10:23:29 +0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/pages/main-page/main-page.component';
-
+import { TaskPageComponent } from './components/pages/tasks-page/task-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
+    TaskPageComponent,
   ],
   imports: [
     BrowserModule,
