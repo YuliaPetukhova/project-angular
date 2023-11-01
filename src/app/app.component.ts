cc410f0e1a43a3ce048c904934f37c0c3556144e
YuliaPetukhova <yuljaschik99@gmail.com>
2023-11-01 10:48:13 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from './components/modal/modal/modal.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
 })
 export class AppComponent {
 
   title = 'Household';
+
+  constructor(private matDialog: MatDialog){}
+
+  openDialog(): void{
+    this.matDialog.open(ModalComponent,{
+      width: '350px',
+    })
+
+  }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/pages/main-page/main-page.component';
 import { TaskPageComponent } from './components/pages/tasks-page/task-page.component';
 import {MatButtonModule } from '@angular/material/button';
@@ -12,17 +12,17 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
 import {MatDialogModule} from '@angular/material/dialog'
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { RouterModule } from '@angular/router';
+ 
 @NgModule({
   declarations: [
-    AppComponent,
     MainPageComponent,
     TaskPageComponent,
     ModalComponent,
     
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     MatButtonModule,
     MatIconModule,
@@ -30,8 +30,13 @@ import {MatDialogModule} from '@angular/material/dialog'
     MatInputModule,
     FormsModule,
     MatDialogModule,
-  ],
+    BrowserAnimationsModule,
+    BrowserModule,
+    // RouterModule.forRoot([
+    //   {path: 'modal', component: ModalComponent}
+    //   ])
+    ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [],
 })
 export class AppModule { }
