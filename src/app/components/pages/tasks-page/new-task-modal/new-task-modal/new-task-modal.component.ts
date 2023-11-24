d44db3a30aa8a622917f317473956ff15b64ecab
YuliaPetukhova <yuljaschik99@gmail.com>
2023-11-24 12:37:18 +0500

    new task from get gist

--- src/app/components/pages/tasks-page/new-task-modal/new-task-modal/new-task-modal.component.ts
+++ src/app/components/pages/tasks-page/new-task-modal/new-task-modal/new-task-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Optional } from '@angular/core';
+import { Component, Inject, OnInit, Optional, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 // import { BrowserModule } from '@angular/platform-browser';
@@ -9,7 +9,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroupDirective,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
@@ -18,43 +24,83 @@ import { MatMenuModule } from '@angular/material/menu';
 import { TaskPageComponent } from '../../task-page.component';
 import { Dialog } from '@angular/cdk/dialog';
 import { TasksService } from 'src/app/services/tasks.service';
+import { ITask } from 'src/app/models/task';
 
 @Component({
   selector: 'app-new-task-modal',
   templateUrl: './new-task-modal.component.html',
   styleUrls: ['./new-task-modal.component.sass'],
 })
-export class NewTaskModalComponent {
+export class NewTaskModalComponent implements OnInit {
   public taskGroups: any;
 
   constructor(
     private tasksService: TasksService,
     public dialogRef: MatDialogRef<NewTaskModalComponent>,
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: { groups: any }
+    @Optional()
+    @Inject(MAT_DIALOG_DATA)
+    public data: { groups: any; tasks: any }
   ) {
     this.taskGroups = window.structuredClone(data).groups;
   }
+  ngOnInit(): void {}
 
-  doAction() {
-	this.tasksService.getAll().subscribe((result) => {
-		console.log(result)
-	  })
-	// let re = this.tasksService.getAll();
-	// console.log(re)
-    // this.tasksService.create({
-    //   id: 3,
-    //   text: 'Your perfect pack for everyday',
-    //   taskGroupId: 1,
-    //   createdAt: '02/11/23',
-    //   doneAt: '05/11/23',
-    //   deletedAt: '06/11/23',
-    //   price: 300,
-    // }).subscribe(()=>{
-	// 	this.dialogRef.close();
-	// });
+  myForm = new FormGroup({
+    taskGroup: new FormControl<number>(0, [Validators.required]),
+    point: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    text: new FormControl<string>('', [Validators.required]),
+  });
 
-    // this.dialogRef.close({ data: this.taskGroups });
+  // @ViewChild('testForm') someNewNameFrom: NgForm | null = null;
+  onSubmit() {
+    console.log('working');
+    // console.log(this.someNewNameFrom);
+    console.log(this.myForm.value);
+
+    let formData: ITask = {
+      text: this.myForm.value.text as string,
+      taskGroupId: this.myForm.value.taskGroup ?? 1,
+      createdAt: '',
+      doneAt: '',
+      deletedAt: '',
+      price: 100,
+    };
+
+    this.tasksService.create(formData).subscribe((result) => {
+      this.dialogRef.close();
+      console.log("result99")
+      console.log(result)
+    });
+
+    this.dialogRef.close({ data: this.taskGroups });
+    console.log('this.data.tasks');
+    console.log(this.data.tasks);
+    
   }
+
+  // doAction() {
+  // this.tasksService.getAll().subscribe((result) => {
+  // 	console.log(result)
+  //   })
+  // let re = this.tasksService.getAll();
+  // console.log(re)
+  // this.tasksService.create(
+
+  //   {
+  //   id: 5,
+  //   text: 'hanuka',
+  //   taskGroupId: 1,
+  //   createdAt: '02/11/23',
+  //   doneAt: '05/11/23',
+  //   deletedAt: '06/11/23',
+  //   price: 300,
+  // }
+  // ).subscribe(()=>{
+
+  // this.dialogRef.close();
+  // });
+  // this.dialogRef.close({ data: this.taskGroups });
+  // }
 
   add(newValue: string): void {
     newValue = newValue.trim();
--- src/app/components/pages/tasks-page/task-page.component.ts
+++ src/app/components/pages/tasks-page/task-page.component.ts
@@ -6,31 +6,46 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 import { NewTaskModalComponent } from './new-task-modal/new-task-modal/new-task-modal.component';
-import { TasksListComponent } from './tasks-list/tasks-list.component'
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { ITask } from 'src/app/models/task';
 import { tasks as data } from 'src/app/data/tasks';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-task-page',
   templateUrl: './task-page.component.html',
   styleUrls: ['./task-page.component.css'],
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule, MatDialogModule, TasksListComponent],
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDialogModule,
+    TasksListComponent,
+  ],
 })
 export class TaskPageComponent {
-  tasks: ITask[] = data;
+  tasks: ITask[];
 
   public groups = [
     { value: 'group-0', viewValue: 'Домашние дела' },
     { value: 'group-1', viewValue: 'Странные дела' },
   ];
 
-  constructor(private matDialog: MatDialog) {}
+  constructor(
+    private matDialog: MatDialog,
+    private tasksService: TasksService
+  ) {
+    tasksService.getAll().subscribe((result) => {
+      this.tasks = result.tasks;
+    });
+  }
 
   openDialog(): void {
     const dialog = this.matDialog.open(NewTaskModalComponent, {
       data: {
         groups: this.groups,
+        tasks: this.tasks
       },
     });
 
