766cdaf65a307b53c891bf8fcdcb0ebdea3e02c7
YuliaPetukhova <yuljaschik99@gmail.com>
2023-12-07 16:40:29 +0500

    crud

--- src/app/components/catalog/tasks-list/task-item/task-item/task-item.component.ts
+++ src/app/components/catalog/tasks-list/task-item/task-item/task-item.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Component, Input } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ITask } from 'src/app/models/task';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-task-item',
@@ -10,24 +12,84 @@ import { ITask } from 'src/app/models/task';
   standalone: true,
   imports: [MatCheckboxModule, CommonModule],
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
+  @ViewChild('readOnlyTemplate', { static: false })
+  readOnlyTemplate: TemplateRef<any>;
+  @ViewChild('editTemplate', { static: false })
+  editTemplate: TemplateRef<any>;
+
   @Input() task: ITask;
 
-  // editedTask: Task;
-	// statusMessage: string;
-	// editTask(task: Task) {
-    //     this.editedTask = new Task(
-		//     task.id,
-		//     task.createdAt,
-		//     task.taskGroupId,
-    //         task.text
-    //     );
-    // }
+  constructor(private serv: TasksService) {
+    this.tasks = new Array<ITask>();
+  }
+  ngOnInit(){
+    this.loadTasks();
+  }
 
-	// deleteTask(task: Task) {
-    //     this.serv.deleteTask(task.id).subscribe((data) => {
-    //         (this.statusMessage = 'Данные успешно удалены'),
-    //             this.loadTask();
-    //     });
-    // }
+  editedTask: ITask;
+  tasks: Array<ITask>;
+  statusMessage: string;
+  isNewRecord: boolean;
+
+  editTask() {
+    this.editedTask = {
+      id: 0,
+      text: 'this.myFormTask.value.text as string',
+      taskGroupId: 1,
+      createdAt: '',
+      doneAt: '',
+      deletedAt: '',
+    };
+    // this.editedTask = new ITask(
+    // task.id,
+    // task.createdAt,
+    // task.taskGroupId,
+    //     task.text
+    // );
+  }
+
+  private loadTasks() {
+    this.serv.getAll().subscribe((data) => {
+        this.tasks = data;
+    });
+}
+// сохраняем пользователя
+saveTask() {
+    if (this.isNewRecord) {
+        // добавляем пользователя
+        this.serv
+            .create(this.editedTask)
+            .subscribe((data) => {
+                (this.statusMessage =
+                    'Данные успешно добавлены'),
+                    this.loadTasks();
+            });
+        this.isNewRecord = false;
+        // this.editedTask = null;
+    } else {
+        // изменяем пользователя
+        this.serv
+            .updateTask(
+                this.editedTask.id as number,
+                this.editedTask
+            )
+            .subscribe((data) => {
+                (this.statusMessage =
+                    'Данные успешно обновлены'),
+                    this.serv.getAll();
+            });
+        // this.editedTask = null;
+    }
+}
+
+deleteTask() {
+    this.serv.deleteTask(this.task.id as number).subscribe((data) => {
+		// const key = this.tasks.filter(task => {
+			// return task.id === this.task.id;
+		// });
+		// this.tasks.removeByKey(key);splice
+      (this.statusMessage = 'Данные успешно удалены'),  this.loadTasks();
+    });
+  }
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,43 +1,36 @@
 import { Injectable } from '@angular/core';
 import { ITask } from '../models/task';
-import {
-  HttpClient,
-  HttpParams,
-} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasksService {
-  private urlAllTasks = 'https://gist.githubusercontent.com/YuliaPetukhova/21340a2637e4a4f0bc6c3fcc7f2c3b75/raw/2771c5bac5003c18ae3e1d7d202c0a669ca41b71/tasks.json';
+  private urlAllTasks =
+    'https://gist.githubusercontent.com/YuliaPetukhova/21340a2637e4a4f0bc6c3fcc7f2c3b75/raw/2771c5bac5003c18ae3e1d7d202c0a669ca41b71/tasks.json';
+  private urlNewTask =
+    'https://gist.githubusercontent.com/YuliaPetukhova/f787a9fee9a088f4a52a6d52451a5d25/raw/42b4035b51de8b5a3bcec1d79458a2963a72a390/new.json';
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get<ITask[]>(
-      this.urlAllTasks,
-      {
-        params: new HttpParams().append('limit', 3),
-      }
-    );
+    return this.http.get<ITask[]>(this.urlAllTasks, {
+      params: new HttpParams().append('limit', 3),
+    });
   }
 
   create(task: ITask): Observable<ITask> {
-    return this.http.get<ITask>(
-      'https://gist.githubusercontent.com/YuliaPetukhova/f787a9fee9a088f4a52a6d52451a5d25/raw/42b4035b51de8b5a3bcec1d79458a2963a72a390/new.json'
-    );
+    return this.http.get<ITask>(this.urlNewTask);
   }
 
-  // updateTask(id: number, task: Task) {
-  //       const urlParams = new HttpParams().set(
-  //           'id',
-  //           id.toString()
-  //       );
-  //       return this.http.put(this.urlAllTasks, task, {
-  //           params: urlParams,
-  //       });
-  //   }
-  //   deleteTask(id: number) {
-  //       return this.http.delete(this.urlAllTasks);
-  //   }
+  updateTask(id: number, task: ITask) {
+    const urlParams = new HttpParams().set('id', id.toString());
+    return this.http.put(this.urlNewTask, task, {
+      params: urlParams,
+    });
+  }
+  deleteTask(id: number) {
+    return this.http.get<ITask>(this.urlNewTask);
+  }
 }
+
