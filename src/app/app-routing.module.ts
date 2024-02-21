6fe5f37d6387e9358d87c2678c8a6d426331ca2f
YuliaPetukhova <petukhova.yulia99@gmail.com>
2024-02-21 17:13:27 +0500

    guard for catalog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {CatalogComponent} from './components/catalog/catalog.component';
 import {IndexComponent} from './components/main/index/index.component';
 
+import { AuthGuard } from './services/auth.guard';
+
+
+
 const routes: Routes = [
   {path: '', component: IndexComponent},
-  {path: 'catalog/:id', component: CatalogComponent},
+  {path: 'catalog/:id', component: CatalogComponent, canActivate: [AuthGuard]},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
